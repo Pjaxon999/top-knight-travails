0c13c9ce3c2b04fb5eaca45906c53598816bc46b
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-05-13 10:33:59 -0600

    initialize project, added notes and readme info

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # top-knight-travails
-TOP project for creating and utilizing graph abstraction
+TOP project for creating and utilizing graph abstraction  
+
+[TOP Project link](https://www.theodinproject.com/lessons/javascript-knights-travails)
---
+++ index.js
@@ -0,0 +1,20 @@
+/* legal moves between the nodes as the moves of the knight, assuming they do 
+not go beyond the bounds of the chessboard
+(x + 2, y + 1), 
+(x - 2, y + 1), 
+(x + 2, y - 1),
+(x - 2, y - 1),
+(x + 1, y + 2),
+(x - 1, y + 2),
+(x + 1, y - 2),
+(x - 1 , y - 2)
+*/
+
+function knightMoves(start, end) {
+    
+}
+
+// Test cases
+knightMoves([0,0],[3,3]); // [[0,0],[2,1],[3,3]]
+knightMoves([3,3],[0,0]); // [[3,3],[2,1],[0,0]]
+knightMoves([0,0],[7,7]); // [[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]]
