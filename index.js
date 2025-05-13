0e70c1ff984b820773e4d1d31ebdb9901672e3f7
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-05-13 11:19:49 -0600

    updated input values and initialized moves array

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ not go beyond the bounds of the chessboard
 (x - 1 , y - 2)
 */
 
-function knightMoves(start, end) {
-    
+function knightMoves(start, target) {
+    // possible moves from any given position
+    const moves = [
+    [2 , 1],
+    [-2 , 1],
+    [2, -1],
+    [-2, -1],
+    [1, 2],
+    [-1, 2],
+    [1, -2],
+    [-1, -2]
+    ];
+
 }
 
 // Test cases
-knightMoves([0,0],[3,3]); // [[0,0],[2,1],[3,3]]
-knightMoves([3,3],[0,0]); // [[3,3],[2,1],[0,0]]
-knightMoves([0,0],[7,7]); // [[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]]
+knightMoves([0,0],[3,3]); // [[0,0],[2,1],[3,3]] or [[0,0],[1,2],[3,3]]
+knightMoves([3,3],[0,0]); // [[3,3],[2,1],[0,0]] or [[3,3],[1,2],[0,0]]
+knightMoves([0,0],[7,7]); // [[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]] or [[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]
